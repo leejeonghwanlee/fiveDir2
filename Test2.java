18e1570cbcce1711787fe0282f8df6baffce8092
leejeonghwanlee <lwalist1204@naver.com>
2019-06-26 16:21:36 +0900

    step1 create Test2.java

---
+++ Test2.java
@@ -0,0 +1,11 @@
+
+Public class Test2{
+	public static void main(String[] args){
+		System.out.println("hello? my name is jeonghwan lee");
+		System.out.println("nice to meet you bro");
+	}
+
+
+
+
+
